6da6528e5270494fb6fe18a2807ad1d55bfc6175
Chris Martin <chris.martin@fourkitchens.com>
2016-03-29 23:33:51 -0500

    Create a full and minimal drupal.make and settings.php

---
+++ aquifer/settings/full.settings.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @file
+ * Drupal site-specific configuration file.
+ */
+
+/**
+ * Include secret configuration.
+ *
+ * Contains database settings and other sensitive environment specific
+ * information that shouldn't be in version control.
+ */
+if (file_exists(DRUPAL_ROOT . '/sites/default/secret.settings.php')) {
+  include DRUPAL_ROOT . '/sites/default/secret.settings.php';
+}
+
+/**
+ * Master module configuration.
+ * @see https://www.drupal.org/project/master
+ */
+$conf['install_profile'] = 'minimal';
+$conf['master_version'] = 2;
+$conf['master_modules'] = array(
+  'base' => array(
+
+    // Core modules.
+    'comment',
+    'contextual',
+    'dblog',
+    'field',
+    'field_sql_storage',
+    'file',
+    'filter',
+    'help',
+    'image',
+    'list',
+    'menu',
+    'node',
+    'number',
+    'options',
+    'path',
+    'rdf',
+    'shortcut',
+    'system',
+    'taxonomy',
+    'text',
+    'update',
+    'url',
+    'user',
+
+    // Contrib modules.
+    'ctools',
+    'diff',
+    'features',
+    'fe_date',
+    'libraries',
+    'master',
+    'pathauto',
+    'search_api',
+    'search_api_solr',
+    'strongarm',
+    'token',
+    'wysiwyg',
+    'views',
+
+    // drupal.make to settings.php
+
+    // Custom modules.
+
+    // Feature modules.
+
+  ),
+  'local' => array(
+    'admin_menu',
+    'devel',
+    'devel_generate',
+    'field_ui',
+    'module_filter',
+  ),
+  'theming' => array(
+    'admin_menu',
+    'themer_devel',
+  )
+  'dev' => array(),
+  'test' => array(),
+  'live' => array(),
+);
+
+$update_free_access = FALSE;
+
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
+
+$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+
+/**
+ * Include local configuration.
+ *
+ * IMPORTANT: This block should remain at the bottom of this file.
+ */
+if (file_exists(DRUPAL_ROOT . '/sites/default/local.settings.php')) {
+  include DRUPAL_ROOT . '/sites/default/local.settings.php';
+}
